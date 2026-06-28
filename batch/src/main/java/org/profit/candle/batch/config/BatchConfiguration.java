bd56a21620c41e66b624b084767e8ae844ad073e
chan-nni <rkdcksal02@gmail.com>
2026-06-29 03:21:04 +0900

    fix(batch): remove custom JobOperator bean

--- batch/src/main/java/org/profit/candle/batch/config/BatchConfiguration.java
+++ batch/src/main/java/org/profit/candle/batch/config/BatchConfiguration.java
@@ -2,10 +2,6 @@ package org.profit.candle.batch.config;
 
 import java.time.Clock;
 import java.time.ZoneId;
-import org.springframework.batch.core.launch.JobOperator;
-import org.springframework.batch.core.launch.support.JobOperatorFactoryBean;
-import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -17,13 +13,5 @@ public class BatchConfiguration {
     @Bean
     public Clock batchClock(BatchProperties batchProperties) {
         return Clock.system(ZoneId.of(batchProperties.schedule().zoneId()));
-    }
-
-    @Bean
-    @ConditionalOnMissingBean(JobOperator.class)
-    public JobOperatorFactoryBean jobOperator(JobRepository jobRepository) {
-        JobOperatorFactoryBean factoryBean = new JobOperatorFactoryBean();
-        factoryBean.setJobRepository(jobRepository);
-        return factoryBean;
     }
 }
