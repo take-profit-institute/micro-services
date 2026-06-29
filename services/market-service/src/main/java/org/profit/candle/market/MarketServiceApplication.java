435658eb1b0ebe9e3946a81abc9b224ed9ba5740
dmstj0162 <dmstj0162@gmail.com>
2026-06-29 17:37:18 +0900

    feat:[Market] modified Implement Kiwoom OAuth client

--- services/market-service/src/main/java/org/profit/candle/market/MarketServiceApplication.java
+++ services/market-service/src/main/java/org/profit/candle/market/MarketServiceApplication.java
@@ -5,12 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MarketServiceApplication {
-    public static void main(String[] args) {
-
-        System.out.println(System.getenv("KIWOOM_APP_KEY"));
-
-        SpringApplication.run(MarketServiceApplication.class, args);
+    public static void main(String[] args) {SpringApplication.run(MarketServiceApplication.class, args);
     }
-
-
 }
