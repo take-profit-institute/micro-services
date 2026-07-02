90b5a2ed3f42f4a2a92b35263edfe832be6ddb88
chan-nni <rkdcksal02@gmail.com>
2026-07-03 02:14:52 +0900

    feat: add ranking profile participant projection

--- services/ranking-service/src/main/java/org/profit/candle/ranking/RankingServiceApplication.java
+++ services/ranking-service/src/main/java/org/profit/candle/ranking/RankingServiceApplication.java
@@ -2,7 +2,9 @@ package org.profit.candle.ranking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
+@EnableKafka
 @SpringBootApplication
 public class RankingServiceApplication {
     public static void main(String[] args) {
---
+++ services/ranking-service/src/main/java/org/profit/candle/ranking/ranking/entity/ParticipantStatus.java
@@ -0,0 +1,9 @@
+package org.profit.candle.ranking.ranking.entity;
+
+public enum ParticipantStatus {
+    UNKNOWN,
+    ACTIVE,
+    INACTIVE,
+    SUSPENDED,
+    DELETED
+}
