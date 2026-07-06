724e74422cf6546146f9d8286a4a7a56b8934a84
yyubin <hazing120@gmail.com>
2026-07-06 21:18:46 +0900

    chore: add tmp for batch image build trigger

--- batch/src/main/java/org/profit/candle/batch/BatchApplication.java
+++ batch/src/main/java/org/profit/candle/batch/BatchApplication.java
@@ -3,6 +3,7 @@ package org.profit.candle.batch;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class BatchApplication {
     public static void main(String[] args) {
